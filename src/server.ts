f53099c4cc3f6c089dd493e06f2baebe2b44f180
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-09-18 20:02:48 -0300

    feat: initialize server with fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import fastify from "fastify";
+
+const app = fastify({
+  logger: true,
+});
+
+try {
+  await app.listen({
+    port: 3000,
+    host: "0.0.0.0",
+  });
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
