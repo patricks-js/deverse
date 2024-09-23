22cd7da1dfe99036811f980b65442c0faf3ba639
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-09-23 12:56:48 -0300

    refactor: organize app main routes

--- apps/web/src/app/(site)/layout.tsx
+++ apps/web/src/app/(site)/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/header";
 import { QueryClientProvider } from "@/shared/query-client-provider";
 import { ThemeProvider } from "@/shared/theme-provider";
 
@@ -6,7 +7,12 @@ export default function SiteLayout({
 }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <QueryClientProvider>{children}</QueryClientProvider>
+      <QueryClientProvider>
+        <div className="min-h-screen px-6 sm:px-8 lg:px-12">
+          <Header />
+          <main className="max-w-screen-md mx-auto pt-6 pb-14">{children}</main>
+        </div>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
--- apps/web/src/app/(site)/page.tsx
+++ apps/web/src/app/(site)/page.tsx
@@ -1,16 +1,3 @@
-import { ArticlesList } from "@/components/articles-list";
-import { Header } from "@/components/header";
-
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <main className="min-h-screen max-w-screen-md mx-auto py-8 px-6 sm:px-8 lg:px-12">
-        <div className="flex items-center justify-between pb-4 border-b-2">
-          <h2 className="text-2xl font-bold tracking-tight">Articles</h2>
-        </div>
-        <ArticlesList />
-      </main>
-    </>
-  );
+  return <div>Feed</div>;
 }
