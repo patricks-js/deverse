6d4d061ce3411ad2350093878ebe11078fae4e66
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-09-24 15:37:12 -0300

    refactor: main site layout

--- apps/web/src/app/(site)/layout.tsx
+++ apps/web/src/app/(site)/layout.tsx
@@ -1,18 +1,12 @@
 import { Header } from "@/components/header";
-import { QueryClientProvider } from "@/shared/query-client-provider";
-import { ThemeProvider } from "@/shared/theme-provider";
 
 export default function SiteLayout({
   children,
 }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <QueryClientProvider>
-        <div className="min-h-screen px-6 sm:px-8 lg:px-12">
-          <Header />
-          <main className="max-w-screen-md mx-auto pt-6 pb-14">{children}</main>
-        </div>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <div className="min-h-screen px-6 max-w-screen-lg mx-auto sm:px-8 lg:px-12">
+      <Header />
+      <main className="max-w-screen-md mx-auto pt-6 pb-14">{children}</main>
+    </div>
   );
 }
