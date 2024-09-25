d44d67a60b12d0a323232167c792d24b7f4b06c0
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-09-25 15:13:44 -0300

    refactor: improve middleware handling with protected routes

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,12 +1,24 @@
 import { auth } from "./lib/auth";
 
+const protectedRoutes = ["/settings", "/editor"];
+
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/sign-in") {
+  const isLoggedIn = !!req.auth;
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    req.nextUrl.pathname.startsWith(route),
+  );
+
+  if (!isLoggedIn && isProtectedRoute) {
     const newUrl = new URL("/sign-in", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 });
 
 export const config = {
-  matcher: ["/settings", "/editor/:path*"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
