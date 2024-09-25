4c1cec8835d68da40c640839774800600206295c
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-09-24 21:48:21 -0300

    fix: site layout width

--- apps/web/src/app/(site)/layout.tsx
+++ apps/web/src/app/(site)/layout.tsx
@@ -6,7 +6,7 @@ export default function SiteLayout({
   return (
     <div className="min-h-screen px-6 max-w-screen-lg mx-auto sm:px-8 lg:px-12">
       <Header />
-      <main className="max-w-screen-md mx-auto pt-6 pb-14">{children}</main>
+      <main className="pt-6 pb-14">{children}</main>
     </div>
   );
 }
